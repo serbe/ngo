bd7c971541dfb5e7f5da843412f97671348b79f8
Serbe <mkoctuk@gmail.com>
2021-04-12 22:17:19 +0300

    add header

--- pages/companies.tsx
+++ pages/companies.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 import { Footer } from '../components/footer'
 import { Header } from '../components/header'
 
-// import { Nav } from '../components/nav'
-
-// import styles from '../styles/Home.module.css'
-
-export default function Home(): JSX.Element {
+export default function Companies(): JSX.Element {
   return (
     <div className="App min-h-full font-custom">
       <Header />
